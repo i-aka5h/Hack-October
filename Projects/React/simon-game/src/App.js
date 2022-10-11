4fca677acdd538f085f6270da41b27c1696fe785
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-11 12:00:17 +0530

    created a heading component and imported some react hooks

--- Projects/React/simon-game/src/App.js
+++ Projects/React/simon-game/src/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    <div className={`w-full h-[100vh] bg-[#011F3F]`}>
+import { useState } from "react"
+import Heading from "./components/Heading";
 
+
+function App() {
+
+  const [level, setLevel] = useState(0);
+  const [heading, setHeading] = useState(`Press Start to start the Game`);
+  return (
+    <div className={`w-full h-[100vh] bg-[#011F3F] text-center`}>
+      <Heading level={level} heading={heading} />
     </div>
   );
 }
---
+++ Projects/React/simon-game/src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Heading = ({level,heading}) => {
+  return (
+      <>
+          <div>
+              Heading
+          </div>
+    </>
+  )
+}
+
+export default Heading
