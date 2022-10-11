f8f2a9639621266353406dbb3d709223e8510576
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-11 12:04:12 +0530

    Styled the heading component and created some states for the heading components with useeffect hook

--- Projects/React/simon-game/src/App.js
+++ Projects/React/simon-game/src/App.js
@@ -1,11 +1,37 @@
-import { useState } from "react"
+// React Imports...
+import { useEffect, useState } from "react"
+
+// App Imports...
 import Heading from "./components/Heading";
 
-
+// App
 function App() {
 
+  // States
   const [level, setLevel] = useState(0);
   const [heading, setHeading] = useState(`Press Start to start the Game`);
+  const [isStarted, setIsStarted] = useState(false)
+
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    // To Update heading when the game starts
+    if (isStarted) {
+      setHeading("Starting...")
+    }
+  }, [isStarted])
+
+
+
+
+
+
   return (
     <div className={`w-full h-[100vh] bg-[#011F3F] text-center`}>
       <Heading level={level} heading={heading} />
--- Projects/React/simon-game/src/components/Heading.js
+++ Projects/React/simon-game/src/components/Heading.js
@@ -1,13 +1,15 @@
-import React from 'react'
 
-const Heading = ({level,heading}) => {
-  return (
-      <>
-          <div>
-              Heading
-          </div>
-    </>
-  )
+const Heading = ({ level, heading }) => {
+    return (
+
+        <h1
+            className={`absolute w-[80vw] leading-9  top-11 md:top-9 lg:top-8 left-1/2 -translate-x-[50%] font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl text-[#fef2bf] items-center`}
+        >
+            {!heading && `Level : ${level}`}
+            {heading}
+        </h1>
+
+    )
 }
 
 export default Heading
