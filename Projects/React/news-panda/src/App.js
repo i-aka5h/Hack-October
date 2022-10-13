b8ff8309b42c5a31e15e236dc0097b84a3b87bdc
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-13 10:08:01 +0530

    Created Navbar, Footer and About components and added thrm to the app

--- Projects/React/news-panda/src/App.js
+++ Projects/React/news-panda/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <About />
+      <Footer />
     </div>
   );
 }
