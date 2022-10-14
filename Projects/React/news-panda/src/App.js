c4c12e95546010e2dd002690e5f77536823bac1f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 13:44:10 +0530

    imported some dependencies, created navbar component included in app.js

--- Projects/React/news-panda/src/App.js
+++ Projects/React/news-panda/src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css';
-import About from './components/About';
-import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <About />
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
