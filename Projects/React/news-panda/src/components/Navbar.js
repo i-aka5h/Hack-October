a8c4c1af1fda19d29a545b824850d1157652311f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 15:01:40 +0530

    Modified  Navbar.js to reuse components

--- Projects/React/news-panda/src/components/Navbar.js
+++ Projects/React/news-panda/src/components/Navbar.js
@@ -1,79 +1,83 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import Logo from "../assets/logos/Logo1.png";
-import { Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const Navbar = (props) => {
-    const location = useLocation();
+const Navbar = ({ title }) => {
+  const newsTypes = [
+    {
+      path: "/",
+      name: "Home"
+    },
+    {
+      path: "/sports",
+      name: "Sports"
+    },
+    {
+      path: "/business",
+      name: "Business"
+    },
+    {
+      path: "/entertainment",
+      name: "Entertainment"
+    },
+    {
+      path: "/health",
+      name: "Health"
+    },
+    {
+      path: "/science",
+      name: "Science"
+    },
+    {
+      path: "/technology",
+      name: "Technology"
+    },
+    {
+      path: "/about",
+      name: "About"
+    }
+  ]
 
-    useEffect(() => {
-      // console.log(location.pathname)
-    }, [location]);
-    
+  const location = useLocation();
+  useEffect(() => {
+  }, [location]);
+
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">
-        <img src={Logo} alt="" />
-        {props.title}
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src={Logo} alt="" />
+          {title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/"?"active":""}` } to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/business"?"active":""}` } to="/business">
-              Business
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/entertainment"?"active":""}` } to="/entertainment">
-              Entertainment
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/health"?"active":""}` } to="/health">
-              Health
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/science"?"active":""}` } to="/science">
-              Science
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/sports"?"active":""}` } to="/sports">
-              Sports
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/technology"?"active":""}` } to="/technology">
-              Technology
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className={`nav-link ${location.pathname==="/about"?"active":""}` } to="/about">
-              About Us
-            </Link>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {
+              newsTypes.map((newsType) => {
+                return (
+                  <li className="nav-item">
+                    <Link className={`nav-link ${location.pathname === newsType.path ? "active" : ""}`} to={newsType.path}>
+                      {newsType.name}
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
