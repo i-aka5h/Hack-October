7de01c808ef0e3d674f464bbddbbbb1cef1fb4e4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 14:36:51 +0530

    created .env for api key, added states for query, added searchinpu component

--- Projects/React/news-panda/src/App.js
+++ Projects/React/news-panda/src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -47,22 +47,46 @@ function App() {
       category: "technology"
     }
   ]
-
+  const API_KEY = process.env.NEWS_API;
   const pageSize = 9;
-  const country = "in";
+  const country = "in";//India
+  const [query, setQuery] = useState("");
+
+  const handleQuery = (event) => {
+    setQuery(event.target.value);
+  };
+
 
 
   return (
     <Router>
       <Navbar />
-      <SearchInput />
+      <SearchInput
+        query={query}
+        handleQuery={handleQuery}
+      />
       <Routes>
+        <Route
+          exact
+          path="/search/:query"
+          element={
+            <SearchNews
+              API_KEY={API_KEY}
+              key="search"
+              pagesize={pageSize}
+              query={query}
+              handleQuery={handleQuery}
+            />
+          }
+        />
+
         {newstypes.map((news) => {
           return (
             <Route exact
               path={news.path}
               element={
                 <News
+                  API_KEY={API_KEY}
                   key={news.key}
                   category={news.category}
                   country={country}
