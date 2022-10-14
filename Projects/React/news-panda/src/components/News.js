9acbe2331de5e22bf18f7be1f22c07946a01d611
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 17:16:03 +0530

    removed console logs added server not found component into news.js

--- Projects/React/news-panda/src/components/News.js
+++ Projects/React/news-panda/src/components/News.js
@@ -3,24 +3,25 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
 import Loading from "./Loading";
+import ServerNotFound from "./ServerNotFound";
 
 
 const News = ({ API_KEY, country, pagesize, category, updateProgress }) => {
-  const capitalize = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
   const [serverNotFound, setServerNotFound] = useState(false);
 
-
+  
+  
+  const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   const loadNews = async () => {
     updateProgress(15);
     const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}&page=${page}&pagesize=${pagesize}`;
-    console.log(url)
     setLoading(true);
     let data = await fetch(url);
     updateProgress(40);
@@ -36,7 +37,6 @@ const News = ({ API_KEY, country, pagesize, category, updateProgress }) => {
       setTotalResults(parsedData.totalResults);
       setLoading(false);
     }
-    console.log(totalResults);
     if (totalResults!==0&& category === "sports") {
       setTotalResults((PrevResult)=>PrevResult-1)
     }
@@ -68,17 +68,8 @@ const News = ({ API_KEY, country, pagesize, category, updateProgress }) => {
   return (
     <>
 
-      {serverNotFound === true ? (
-        <div className="container px-3 my-5" style={{ maxWidth: "60vw" }}>
-          <div className="card text-white bg-dark ">
-            <div className="card-header pl-4">#Server Not Found</div>
-            <div className="card-body text-center">
-              <h5 className="card-title ">Opps...</h5>
-              <p className="card-text ">No News Available Right Now,</p>
-              <p className="card-text ">Please try after some time...</p>
-            </div>
-          </div>
-        </div>
+      {serverNotFound ? (
+        <ServerNotFound/>
       ) : (
         <div className="container my-8">
           <h2 className="text-center my-4">
