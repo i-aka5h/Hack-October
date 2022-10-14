6ad56738e55fc39c29f51f20d94dc2f17fc877fb
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 16:08:50 +0530

    Created Loading Component

--- Projects/React/news-panda/src/components/Loading.js
+++ Projects/React/news-panda/src/components/Loading.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import loading from "../assets/gifs/Spinner_black_3.gif";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
-
-export default Loading
+    <div className="text-center">
+      <img src={loading} alt="Loading" />
+    </div>
+  );
+};
+export default Loading;
