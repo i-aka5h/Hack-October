e99c306ad21a1b3b22557a32e07c9946fd6b8ec1
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 14:12:04 +0530

    Created Routes for all the pages

--- Projects/React/news-panda/src/App.js
+++ Projects/React/news-panda/src/App.js
@@ -1,11 +1,82 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import News from "./components/News";
+import SearchNews from "./components/SearchNews";
+import SearchInput from "./components/SearchInput";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+
+  const newstypes = [
+    {
+      key: "general",
+      path: "/",
+      category: "general"
+
+    },
+    {
+      key: "sports",
+      path: "/sports",
+      category: "sports"
+    },
+    {
+      key: "business",
+      path: "business",
+      category: "business"
+    },
+    {
+      key: "entertainment",
+      path: "entertainment",
+      category: "entertainment"
+    },
+    {
+      key: "health",
+      path: "health",
+      category: "health"
+    },
+    {
+      key: "science",
+      path: "science",
+      category: "science"
+    },
+    {
+      key: "technology",
+      path: "technology",
+      category: "technology"
+    }
+  ]
+
+  const pageSize = 9;
+  const country = "in";
+
+
   return (
     <Router>
       <Navbar />
+      <SearchInput />
+      <Routes>
+        {newstypes.map((news) => {
+          return (
+            <Route exact
+              path={news.path}
+              element={
+                <News
+                  key={news.key}
+                  category={news.category}
+                  country={country}
+                  pageSize={pageSize}
+                />
+              }
+            />
+          )
+        })}
+
+        <Route exact path="/about" element={<About />} />
+
+      </Routes>
+      <Footer />
     </Router>
   );
 }
