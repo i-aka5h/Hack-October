ae7e673801e4835fe84d0b8aeb704bf86fb772e3
Abhishek <abhishek2001rai@gmail.com>
2022-10-22 22:26:55 +0530

    added another one

---
+++ Projects/Another one/javascript.js
@@ -0,0 +1,41 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    }
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    else if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            return "scissors wins";
+        }
+        else {
+            return "rock wins";
+        }
+    }
+};
+
+console.log(compare(userChoice,computerChoice));
