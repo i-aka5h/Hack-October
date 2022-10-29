e71b0b9867c42a6f8bfd3cb68250eaf1ce4d5deb
kashyap5320 <63612972+kashyap5320@users.noreply.github.com>
2022-10-29 01:39:06 -0400

    origin

---
+++ python/loop.py
@@ -0,0 +1,21 @@
+t=10
+print("Display all the even numbers between 10 and 100")
+while(t<=100):
+    print(t,end =" ")
+    t+=2
+print()
+print("Display all Odd numbers Backward from 999 to 777")
+for i in range(999, 777, -2):
+    print(i, end=" ")
+print()
+print("Using the While Loop, display all numbers moving by doubles starting from 20 to 50,000, such as:")
+t=20
+while(t<50000):
+    print(t,end=" ")
+    t=t*2
+print()
+t=int(input("Enter the number of time user want to repeat the string"))
+while(t>0):
+    print("Programming Python")
+    t-=1
+t=Tr
